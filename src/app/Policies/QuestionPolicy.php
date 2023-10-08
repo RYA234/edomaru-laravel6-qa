164849d9f2f664f8985482c3014d56d4ca313ea2
RYA234 <mryuabc@gmail.com>
2023-10-08 12:19:58 +0900

    148. Saving The Answer

--- src/app/Policies/QuestionPolicy.php
+++ src/app/Policies/QuestionPolicy.php
@@ -3,6 +3,7 @@
 namespace App\Policies;
 
 use App\User;
+use App\Question;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
 class QuestionPolicy
@@ -26,6 +27,6 @@ class QuestionPolicy
 
     public function delete(User $user, Question $question)
     {
-        return $user->id=== $question->user_id && $question->answers < 1;
+        return $user->id=== $question->user_id && $question->answers_count < 1;
     }
 }
