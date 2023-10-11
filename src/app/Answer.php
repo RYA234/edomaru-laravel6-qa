289632ab92bbbc6a529db081aadd9a5795bbea54
RYA234 <mryuabc@gmail.com>
2023-10-12 07:26:59 +0900

    152.Update best_answer_id on questions table when an answer removed using

--- src/app/Answer.php
+++ src/app/Answer.php
@@ -24,14 +24,29 @@ class Answer extends Model
     public static function boot()
     {
         parent::boot();
+
         static::created(function($answer){
             $answer->question->increment('answers_count');
-            $answer->question->save();
+        });
+
+        static::deleted(function ($answer){
+            $question = $answer->question;
+            $answer->question->decrement('answers_count');
+            if($question->best_answer_id === $answer->id)
+            {
+                $question->best_answer_id = null;
+                $question->save();
+            }
         });
     }
 
     public function getCreatedDataAttribute()
     {
         return $this->created_at->diffForHumans();
+    }
+
+    public function getStatusAttribute()
+    {
+        return $this->id === $this->question->best_answer_id ? 'vote-accepted' : '';
     }
 }
